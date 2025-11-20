c0ef15b41dec824561cac0e9502ace51cc602820
Velraj-Murugesan <velraj.murugesan1991@gmail.com>
2025-11-20 10:29:22 +0530

    Day2 commit on varaible

---
+++ Day2.py
@@ -0,0 +1,17 @@
+a = 1
+b = 1.1
+c = 'velraj'
+d = True
+e = None
+f: list[object] = [1, 2.2, True, 'Velraj']
+g = (3, 4.4, 'world', True)
+h: dict[str, str | int | bool] = {
+    'name': 'velraj', 'age': 25, 'is_student': False}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
