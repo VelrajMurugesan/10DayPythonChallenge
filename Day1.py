ef31aa36b3ce4bb39fc488a346eedb1dce4b650b
Velraj-Murugesan <velraj.murugesan1991@gmail.com>
2025-11-20 09:56:12 +0530

    day1 file commit

---
+++ Day1.py
@@ -0,0 +1,19 @@
+print("Welcome to taste success of AI agent benefits!!!")
+
+
+# print("Welcome to taste success of AI agent benefits!!!")
+""" print("Welcome to taste success of AI agent benefits!!!")
+print("Welcome to taste success of AI agent benefits!!!") """
+
+a = 20
+b = 10
+c = a
+
+print(c)
+
+
+print("Sum of a & b : ", a+b)
+print("Sub of a & b : ", a-b)
+print("mul of a & b : ", a*b)
+print("Module of a & b: ", a % b)      # modulus operator
+print("Divide of a & b: ", a//b)     # floor division
