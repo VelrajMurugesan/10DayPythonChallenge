f9f2aae98425bfb68705a58786447c264b7995ee
Velraj-Murugesan <velraj.murugesan1991@gmail.com>
2025-11-21 09:33:54 +0530

    Day3 Nested if condition

--- Day3.py
+++ Day3.py
@@ -1,14 +1,17 @@
+# Grade Calculator
 score = int(input("Enter a exam mark : "))
 
-if score <= 100 and score >= 90:
-    print("A")
-elif score < 90 and score >= 80:
-    print("B")
-elif score < 80 and score >= 70:
-    print("C")
-elif score < 70 and score >= 60:
-    print("D")
-elif score < 60 and score >= 0:
-    print("F")
+if score > 60:
+    print("Pass")
+    if score <= 100 and score >= 90:
+        print("And Your Grade is : A")
+    elif score < 90 and score >= 80:
+        print("And Your Grade is : B")
+    elif score < 80 and score >= 70:
+        print("And Your Grade is : C")
+    elif score < 70 and score >= 60:
+        print("And Your Grade is : D")
+    else:
+        print("Invalid mark")
 else:
-    print("Invalid mark")
+    print("Fail")
